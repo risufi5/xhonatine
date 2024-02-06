28bf0113e37c9dab7973a6332abb774f7fb6f4ac
renisisufi <renis.isufi5@gmail.com>
2024-02-06 01:42:01 +0100

    Final code done

---
+++ js/script.js
@@ -0,0 +1,73 @@
+function moveNoButton() {
+    var noButton = document.getElementById('noButton');
+    noButton.style.position = 'absolute';
+    noButton.style.left = Math.random() * (window.innerWidth - 100) + 'px';
+    noButton.style.top = Math.random() * (window.innerHeight - 50) + 'px';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Script loaded!');
+});
+
+function playOnPageLoad() {
+    let audioElement = document.getElementById('audioFirst');
+    audioElement.play();
+}
+
+function playAudio(audioId) {
+    var audioElement = document.getElementById(audioId);
+
+    // Pause all other audio elements
+    var allAudioElements = document.querySelectorAll('audio');
+    allAudioElements.forEach(function (element) {
+        if (element.id !== audioId) {
+            element.pause();
+        }
+    });
+
+    // Play the selected audio
+    audioElement.play();
+}
+
+function sheSaidYes() {
+    var yesButton = document.getElementById('yesButton');
+    yesButton.addEventListener('click', playAudio('audioWhenClickYes'));
+    
+    // Remove no button
+    var noButton = document.getElementById('noButton');
+    noButton.style.display = 'none';
+
+    // Remove the question container
+    var questionContainer = document.getElementById('questionContainer');
+    if (questionContainer) {
+        questionContainer.style.display = 'none';
+    }
+    
+    document.body.style.backgroundImage = 'url("../image/ily.png")';
+    var lovePage = document.getElementById('love');
+    lovePage.style.display = 'block';
+}
+
+function sheSaidNo() {
+        var noButton = document.getElementById('noButton');
+        noButton.addEventListener('click', playAudio('audioWhenClickNo'));
+        moveNoButton()
+}
+
+function nextToQuestion() {
+    var firstPage = document.getElementById('questionContainer0');
+    firstPage.style.display = 'none';
+    document.body.style.backgroundImage = 'url("../image/backgroundCouple.jpg")';
+    var secondPage = document.getElementById('questionContainer');
+    secondPage.style.display = 'block';
+    playOnPageLoad();
+}
+
+function love() {
+    var lovePage = document.getElementById('love');
+    lovePage.style.display = 'none';
+    document.body.style.backgroundImage = 'url("../image/love.JPEG")';
+    setTimeout(() => {
+        document.body.style.opacity = '1';  // Fade-in
+    }, 100);  // Delay for smoother transition
+}
